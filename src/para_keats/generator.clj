5914e785c417c2d5d8ccb3209432935a5416d5a8
Alex F <adfarries@gmail.com>
2018-07-22 15:51:31 +1000

    Add fn for creating set of prefixes with suffixes

--- src/para_keats/generator.clj
+++ src/para_keats/generator.clj
@@ -1,3 +1,15 @@
 (ns para-keats.generator)
 
 (def test-string "There is not a fiercer hell than the failure in a great object.")
+
+(def words (clojure.string/split test-string #" "))
+
+(def word-transitions (partition-all 3 1 words))
+
+(reduce (fn [p s]
+          (merge-with clojure.set/union p
+                      (let [[a b c] s]
+                        {[a b]
+                         (if c #{c} #{})})))
+        {}
+        word-transitions)
