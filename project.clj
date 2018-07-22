d8edca60c01e33a7cbf32e028666379dff37c557
Alex F <adfarries@gmail.com>
2018-07-22 16:20:12 +1000

    Update project.clj with description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject para-keats "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "John Keats mashup (http://howistart.org/posts/clojure/1/index.html)"
+  :url "https://github.com/cursande/para-keats"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
