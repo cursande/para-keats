f856021b339acef5218a52ae61e68d5eb3a97d24
Alex F <adfarries@gmail.com>
2018-07-22 17:03:47 +1000

    Add word-chain + spec
    * Will modify word-chain to behave differently later on

--- test/para_keats/generator_test.clj
+++ test/para_keats/generator_test.clj
@@ -1,3 +1,14 @@
 (ns para-keats.generator-test
   (:require [clojure.test :refer :all]
             [para-keats.generator :refer :all]))
+
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions between suffixes and prefixes"
+    (let [example '(("And" "the" "Golden")
+                    ("the" "Golden" "Grouse")
+                    ("And" "the" "Pobble")
+                    ("the" "Pobble" "who"))]
+      (is (= {["the" "Pobble"] #{"who"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (word-chain example))))))
