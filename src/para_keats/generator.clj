20cecc0adb0d33a78ba67e5ade69193e14d9d69e
Alex F <adfarries@gmail.com>
2018-08-05 15:36:01 +1000

    Split lines on punct + newline
    * Remove empty strings from lazy seq

--- src/para_keats/generator.clj
+++ src/para_keats/generator.clj
@@ -1,9 +1,5 @@
 (ns para-keats.generator)
 
-(def words (clojure.string/split test-string #" "))
-
-(def word-transitions (partition-all 3 1 words))
-
 (defn word-chain [word-transitions]
   (reduce (fn [p s]
             (merge-with clojure.set/union p
@@ -11,3 +7,18 @@
                           {[a b] (if c #{c} #{})})))
           {}
           word-transitions))
+
+(defn text->word-swap [s]
+  (let [lines (remove (fn [s] (= s "")) (clojure.string/split s #"[;|,|\n]"))
+        last-words (clojure.string/split lines #" ")]
+    last-words))
+
+(def test-stanza
+  "Season of mists and mellow fruitfulness,
+  Close bosom-friend of the maturing sun;
+  Conspiring with him how to load and bless
+  With fruit the vines that round the thatch-eves run;")
+
+(text->word-swap test-stanza)
+
+(remove (fn [s] (= s "")) (clojure.string/split test-stanza #"[;|,|\n]"))
