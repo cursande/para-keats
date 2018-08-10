1d3a9ea329dd23fbe058bf9d97e568f5e814418c
Alex F <adfarries@gmail.com>
2018-08-10 23:24:45 +1000

    Add fetch-rhymes function

--- src/para_keats/generator.clj
+++ src/para_keats/generator.clj
@@ -1,24 +1,31 @@
 (ns para-keats.generator
-  (:use [clojure.string :only (split)]))
+  (:require [clj-http.client :as client]
+            [clojure.string :refer [split]]
+            [cheshire.core :refer [parse-string]]))
 
-
-(defn word-chain [word-transitions]
-  (reduce (fn [p s]
-            (merge-with clojure.set/union p
-                        (let [[a b c] s]
-                          {[a b] (if c #{c} #{})})))
-          {}
-          word-transitions))
 
 (defn text->word-swap [s]
-  (let [lines (into [] (remove (fn [s] (= s "")) (split s #"[;|,|\n]")))
-        last-words (map (fn [s] (last (split s #" "))) lines)]
-    last-words))
+  (let [lines (into [] (remove (fn [s] (= s "")) (split s #"[;|,|\n]")))]
+    (map (fn [s] (last (split s #" ")))
+         lines)))
+
+(defn fetch-rhymes [last-words]
+  (let [base-url "https://api.datamuse.com/words?rel_rhy="]
+    (map (fn [word]
+           (as-> [word] res
+             (:body (client/get (str base-url word) {:as :json}))
+             (if (empty? res)
+               word
+               (:word (rand-nth res)))))
+         last-words)))
+
 
 (def test-string
   "Season of mists and mellow fruitfulness,
   Close bosom-friend of the maturing sun;
   Conspiring with him how to load and bless
   With fruit the vines that round the thatch-eves run;")
+
+(fetch-rhymes (text->word-swap test-string))
 
 (text->word-swap test-string)
