9a7e2f8cd1aefcfef5e26417557bf3175691a803
Alex F <adfarries@gmail.com>
2018-08-10 23:24:16 +1000

    Add clj-http and cheshire

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/cursande/para-keats"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.0"]]
   :profiles {:dev
              {:plugins [[com.jakemccrary/lein-test-refresh "0.7.0"]]}})
