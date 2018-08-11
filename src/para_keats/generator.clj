ea38e49edb202732e3d094e2fee1d7adaf2dd263
Alex F <adfarries@gmail.com>
2018-08-11 20:44:59 +1000

    Move word-swap to bottom of file
    * makes it easier to refresh the ns

--- src/para_keats/generator.clj
+++ src/para_keats/generator.clj
@@ -3,13 +3,6 @@
             [clojure.string :refer [split replace blank?]]
             [cheshire.core :refer [parse-string]]))
 
-; TODO Set up regex search for word + ;|,|\n, rather than just searching for the word on its own
-(defn word-swap [text]
-  (let [last-words (text->last-words text)
-        with-rhymes (map (fn [a, b] [a, b])
-                         last-words (fetch-rhymes last-words))]
-    (reduce (fn [s, r] (apply replace s r)) text with-rhymes)))
-
 (defn text->last-words [s]
   (let [lines (into [] (remove (fn [s] (blank? s)) (split s #"[;|,|\n]")))]
     (map (fn [s] (last (split s #" ")))
@@ -24,3 +17,11 @@
                word
                (:word (rand-nth res)))))
          last-words)))
+
+; TODO Set up regex search for word + ;|,|\n, rather than just searching for the word on its own
+(defn word-swap [text]
+  (let [last-words (text->last-words text)
+        with-rhymes (map (fn [a, b] [a, b])
+                         last-words (fetch-rhymes last-words))]
+    (reduce (fn [s, r] (apply replace s r)) text with-rhymes)))
+
